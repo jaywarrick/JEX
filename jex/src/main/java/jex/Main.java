072e2cbbd27f76be00c7b9ae06bc703202d2086b
Curtis Rueden <ctrueden@wisc.edu>
2014-06-23 12:17:25 -0500

    Add a toplevel application module
    This module is intended to be a single dependency for anyone who wants
    to depend on JEX, as well as a main method entry point.
    
    It declares the core JEX library itself (jex-core) as a compile scope
    dependency, and declares all the optional plugins as runtime scope so
    that they are available (even though they are technically not needed to
    actually build the code).

---
+++ jex/src/main/java/jex/Main.java
@@ -0,0 +1,10 @@
+package jex;
+
+import jex.JExperiment;
+
+/** Main entry point into JEX. */
+public class Main {
+  public static void main(final String[] args) {
+    JExperiment.main(args);
+  }
+}
