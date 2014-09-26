f16bab6631ffe115a38dffa6cd3b2c1f91bfcb93
jaywarrick <jaywarrick@Warrick-Work.local>
2014-09-26 07:10:11 -0500

    Create JEXPlugin for analyzing microwell secretion
    Also includes helper class called MicrowellAnalyzer where potentially
    reusable static methods reside.

---
+++ core/src/main/java/function/plugin/plugins/singleCell/MeasureSecretion.java
@@ -0,0 +1,195 @@
+package function.plugin.plugins.singleCell;
+
+import ij.ImagePlus;
+import ij.process.ImageProcessor;
+import image.roi.IdPoint;
+import image.roi.PointList;
+import image.roi.ROIPlus;
+
+import java.util.TreeMap;
+
+import jex.statics.JEXStatics;
+import logs.Logs;
+import miscellaneous.Pair;
+
+import org.scijava.plugin.Plugin;
+
+import tables.DimTable;
+import tables.DimensionMap;
+import weka.core.converters.JEXTableWriter;
+import Database.DBObjects.JEXData;
+import Database.DBObjects.JEXEntry;
+import Database.DataReader.ImageReader;
+import Database.DataReader.RoiReader;
+import Database.DataWriter.FileWriter;
+import Database.DataWriter.ImageWriter;
+import function.plugin.mechanism.InputMarker;
+import function.plugin.mechanism.JEXPlugin;
+import function.plugin.mechanism.MarkerConstants;
+import function.plugin.mechanism.OutputMarker;
+import function.plugin.mechanism.ParameterMarker;
+
+/**
+ * This is a JEXperiment function template To use it follow the following instructions
+ * 
+ * 1. Fill in all the required methods according to their specific instructions 2. Place the file in the Functions/SingleDataPointFunctions folder 3. Compile and run JEX!
+ * 
+ * JEX enables the use of several data object types The specific API for these can be found in the main JEXperiment folder. These API provide methods to retrieve data from these objects, create new objects and handle the data they contain.
+ * 
+ * @author erwinberthier
+ * 
+ */
+
+@Plugin(
+		type = JEXPlugin.class,
+		name="Measure Secretion",
+		menuPath="Single Cell",
+		visible=true,
+		description="Quantify captured secretion on beads in microwells by determining 'secretion per bead'."
+		)
+public class MeasureSecretion extends JEXPlugin {
+
+	public MeasureSecretion()
+	{}
+	
+	/////////// Define Inputs ///////////
+	
+	@InputMarker(name="Image", type=MarkerConstants.TYPE_IMAGE, description="Image to be quantified.", optional=false)
+	JEXData imageData;
+	
+	@InputMarker(name="Microwell Locations", type=MarkerConstants.TYPE_ROI, description="ROI of microwell locations.", optional=false)
+	JEXData roiData;
+	
+	@InputMarker(name="Enhanced BF Image", type=MarkerConstants.TYPE_IMAGE, description="Brightfield image that can help determine which image items are beads and not random fluorescent particles", optional=true)
+	JEXData bfData;
+	
+	/////////// Define Parameters ///////////
+	
+	@ParameterMarker(uiOrder=1, name="Color Dim Name", description="Name of the 'Color' dimension", ui=MarkerConstants.UI_TEXTFIELD, defaultText="Color")
+	String colorDimName;
+	
+	@ParameterMarker(uiOrder=2, name="Secretion Color Dim", description="Value of the 'Color' dim that represents the secretion signal to be quantified.", ui=MarkerConstants.UI_TEXTFIELD, defaultText="0")
+	String secretionColor;
+	
+	@ParameterMarker(uiOrder=3, name="Bead Color Dim", description="Value of the 'Color' dim that represents the bead signal used as reference for quantifying secretion signal.", ui=MarkerConstants.UI_TEXTFIELD, defaultText="1")
+	String beadColor;
+	
+	@ParameterMarker(uiOrder=4, name="Microwell Width", description="Width of the microwell in pixels.", ui=MarkerConstants.UI_TEXTFIELD, defaultText="100")
+	double width;
+	
+	@ParameterMarker(uiOrder=5, name="Microwell Height", description="Height of the microwell in pixels", ui=MarkerConstants.UI_TEXTFIELD, defaultText="100")
+	double height;
+	
+	@ParameterMarker(uiOrder=6, name="Create Scatter Plots", description="Whether to create plots for each microwell showing the secretion to bead signal ratios of each pixel for each microwell.", ui=MarkerConstants.UI_CHECKBOX, defaultBoolean=false)
+	boolean plot;
+	
+	@ParameterMarker(uiOrder=7, name="Enhanced BF Threshold", description="Threshold to be considered a bead.", ui=MarkerConstants.UI_TEXTFIELD, defaultText="100")
+	double bfThresh;
+	
+	@ParameterMarker(uiOrder=8, name="Scale Enhanced BF Microwell ROI", description="Amount to scale the Microwell ROI to match the enhance BF image.", ui=MarkerConstants.UI_TEXTFIELD, defaultText="0.5")
+	double bfRoiScale;
+	
+	/////////// Define Outputs ///////////
+	
+	@OutputMarker(name="Microwell Plots", type=MarkerConstants.TYPE_IMAGE, flavor="", description="Plots of secretion to bead signal ratios for each microwell.", enabled=true)
+	JEXData plots;
+	
+	@OutputMarker(name="Ratio Data", type=MarkerConstants.TYPE_FILE, flavor="", description="Table of secretion to bead signal ratios for each microwell.", enabled=true)
+	JEXData ratios;
+	
+	@Override
+	public int getMaxThreads()
+	{
+		return 1; // R doesn't like multiple threads.
+	}
+
+	@Override
+	public boolean run(JEXEntry optionalEntry)
+	{
+		imageData.getDataMap();
+		if(imageData == null || !imageData.getTypeName().getType().equals(JEXData.IMAGE))
+		{
+			return false;
+		}
+		
+		roiData.getDataMap();
+		if(roiData == null || !roiData.getTypeName().getType().equals(JEXData.ROI))
+		{
+			return false;
+		}
+		
+		boolean haveBF = true;
+		if(bfData == null || !bfData.getTypeName().getType().equals(JEXData.IMAGE))
+		{
+			haveBF = false;
+		}
+		
+		// Run the function
+		TreeMap<DimensionMap,String> imageMap = ImageReader.readObjectToImagePathTable(imageData);
+		TreeMap<DimensionMap,ROIPlus> roiMap = RoiReader.readObjectToRoiMap(roiData);
+		TreeMap<DimensionMap,String> bfMap = null;
+		if(haveBF)
+		{
+			bfMap = ImageReader.readObjectToImagePathTable(bfData);
+		}
+		TreeMap<DimensionMap,String> outputPlotMap = new TreeMap<DimensionMap,String>();
+		TreeMap<DimensionMap,Double> outputDataMap = new TreeMap<DimensionMap,Double>();
+		
+		int count = 0, percentage = 0;
+		DimensionMap beadMap;
+		ROIPlus temp;
+		DimTable table = imageData.getDimTable().getSubTable(new DimensionMap(colorDimName + "=" + secretionColor));
+		for (DimensionMap secretionMap : table.getMapIterator())
+		{
+			beadMap = secretionMap.copyAndSet(colorDimName + "=" + beadColor);
+			ROIPlus roi = roiMap.get(secretionMap);
+			PointList pl = roi.getPointList();
+			ImageProcessor sIm = new ImagePlus(imageMap.get(secretionMap)).getProcessor();
+			ImageProcessor bIm = new ImagePlus(imageMap.get(beadMap)).getProcessor();
+			ImageProcessor bfIm = null;
+			if(haveBF)
+			{
+				bfIm = new ImagePlus(bfMap.get(secretionMap)).getProcessor();
+			}
+			for(IdPoint p : pl)
+			{
+				if(this.isCanceled())
+				{
+					return false;
+				}
+				DimensionMap dataMap = secretionMap.copyAndSet("Microwell=" + p.id);
+				temp = ROIPlus.makeRoi(p.x, p.y, ROIPlus.ORIGIN_CENTER, ROIPlus.ROI_RECT, (int) width, (int) height);
+				Pair<String,Double> result = MicrowellAnalyzer.getSig1ToSig2Ratio(sIm, bIm, p, temp, plot, bfIm, bfThresh, bfRoiScale);
+				if(result != null)
+				{
+					if(plot)
+					{
+						outputPlotMap.put(dataMap, result.p1);
+					}
+					outputDataMap.put(dataMap, result.p2);
+				}
+				else
+				{
+					Logs.log("To few detectable beads to quantify for this microwell: " + p.id + " in image " + secretionMap.toString(), this);
+				}
+			}
+			count = count + 1;
+			percentage = (int) (100 * ((double) (count) / ((double) table.mapCount())));
+			JEXStatics.statusBar.setProgressPercentage(percentage);
+		}
+		if(outputDataMap.size() == 0)
+		{
+			return false;
+		}
+		
+		if(plot)
+		{
+			this.plots = ImageWriter.makeImageStackFromPaths("temp",outputPlotMap);
+		}
+		String tablePath = JEXTableWriter.writeTable("SecretionToBeadRatio", outputDataMap, "arff");
+		this.ratios = FileWriter.makeFileObject("temp", null, tablePath);
+		
+		// Return status
+		return true;
+	}
+}
---
+++ core/src/main/java/function/plugin/plugins/singleCell/MicrowellAnalyzer.java
@@ -0,0 +1,163 @@
+package function.plugin.plugins.singleCell;
+
+import ij.process.ImageProcessor;
+import image.roi.IdPoint;
+import image.roi.PointList;
+import image.roi.ROIPlus;
+
+import java.awt.Rectangle;
+
+import logs.Logs;
+import miscellaneous.Pair;
+import miscellaneous.StatisticsUtility;
+import rtools.R;
+
+
+public class MicrowellAnalyzer {
+	
+	
+	public static Pair<String,Double> getSig1ToSig2Ratio(ImageProcessor image1, ImageProcessor image2, IdPoint microwell, ROIPlus template, boolean plot, ImageProcessor bfIm, double bfThresh, double bfRoiScale)
+	{
+		PointList pl = template.getPointList();
+		pl.setCenter(microwell.x, microwell.y);
+		Rectangle r = pl.getBounds();
+		int n = r.width*r.height;
+		int xMax = Math.max(image1.getWidth(), image2.getWidth());
+		int yMax = Math.max(image1.getHeight(), image2.getHeight());
+		boolean haveBF = (bfIm != null);
+		
+		// Get the data from the images
+		double[] sig1 = new double[n];
+		double[] sig2 = new double[n];
+		double[] bf = new double[n];
+		int i = 0;
+		for(int x=r.x; x<(r.x+r.width); x++)
+		{
+			for(int y=r.y; y<(r.y+r.width); y++)
+			{
+				if(x >= 0 && x < xMax && y >= 0 && y < yMax)
+				{
+					sig1[i] = image1.getPixelValue(x, y);
+					sig2[i] = image2.getPixelValue(x, y);
+					if(haveBF)
+					{
+						bf[i] = bfIm.getPixelValue((int)Math.round(x*bfRoiScale), (int)Math.round(y*bfRoiScale));
+					}
+					i++;
+				}
+				else
+				{
+					Logs.log("Index out of bounds!!", Logs.ERROR, MicrowellAnalyzer.class);
+				}
+			}
+		}
+		
+		boolean [] bfFilterHi = null;
+		boolean [] bfFilterLo = null;
+		double[] sig1BG = null;
+		double[] sig2BG = null;
+		if(haveBF)
+		{
+			bfFilterHi = StatisticsUtility.getThresholdFilter(bf, bfThresh, true, false);
+			bfFilterLo = StatisticsUtility.getThresholdFilter(bf, bfThresh, false, true);
+			sig1BG = StatisticsUtility.applyFilter(sig1, bfFilterLo); // Make sure we are working with BG pixels
+			sig2BG = StatisticsUtility.applyFilter(sig2, bfFilterLo); // Make sure we are working with BG pixels
+			sig1 = StatisticsUtility.applyFilter(sig1, bfFilterHi); // Make sure we are working with pixels where there is a bead or something
+			sig2 = StatisticsUtility.applyFilter(sig2, bfFilterHi); // Make sure we are working with pixels where there is a bead or something
+		}
+		else
+		{
+			sig1BG = sig1;
+			sig2BG = sig2;
+		}
+		
+		double xMad = StatisticsUtility.mad(sig2BG);
+		double yMad = StatisticsUtility.mad(sig1BG);
+		double xMed = StatisticsUtility.median(sig2BG);
+		double yMed = StatisticsUtility.median(sig1BG);
+		double xThresh = xMed + 4*xMad;
+		double yThresh = yMed + 4*yMad;
+		boolean[] xFilter = StatisticsUtility.getThresholdFilter(sig2, xThresh, true, false); // Make sure we are working with pixels where there is a bead (this is a different measure of 'beadness')
+		
+		double[] xFiltered = StatisticsUtility.applyFilter(sig2, xFilter);
+		double[] yFiltered = StatisticsUtility.applyFilter(sig1, xFilter);
+		
+		
+		// Get the linear model
+		// We want sig1/sig2 ratio so because we calculate dy/dx, we treat sig1 as y and sig2 as x
+		
+		if(xFiltered.length > 10)
+		{
+			double rise = StatisticsUtility.mean(yFiltered) - yMed;
+			double run = StatisticsUtility.mean(xFiltered) - xMed;
+			double slope = rise/run;
+			double yint = yMed - slope*xMed;
+			double[] fit = new double[]{yint, slope};
+			
+			String path = null;
+			if(plot)
+			{
+				path = plotPixels(microwell.id, sig2BG, sig1BG, sig2, sig1, xThresh, yThresh, fit);
+			}
+			
+			return new Pair<String, Double>(path, fit[1]);
+			
+			//			LinearModel lm = new LinearModel(xFiltered, yFiltered);
+			//			lm.compute();
+			//			
+			//			// returns {intercept, slope} so grab element at index 1 for slope (i.e. ratio)
+			//			double[] fit = lm.getCoefficients();
+			//			
+			//			String path = null;
+			//			if(plot)
+			//			{
+			//				path = plotPixels(microwell.id, sig2, sig1, xThresh, yThresh, fit);
+			//			}
+			//			
+			//			return new Pair<String, Double>(path, fit[1]);
+		}
+		else
+		{
+			return null;
+		}
+	}
+	
+	public static String plotPixels(int id, double[] xBG, double[] yBG, double[] x, double[] y, double xThresh, double yThresh, double[] fit)
+	{
+		String path = R.startPlot("tif", 4, 3, 150, 12, R.FONT_HELVETICA, R.COMPRESSION_NONE);
+		
+		R.eval("'Plotting pixel data'");
+		
+		// Make the plot somewhat prettier
+		R.eval("par(mar = c(3.7,3.7,0.8,0.8)) # beyond plot frame [b,l,t,r]");
+		R.eval("par(mgp = c(2.5,0.8,0)) # placement of axis labels [1], numbers [2] and symbols[3]");
+		R.eval("par(oma = c(0.1,0.1,0.1,0.1)) # cutoff beyond other measures");
+		R.eval("par(cex = 1.25)");
+		R.eval("par(cex.lab = 0.75) # axislabelsize");
+		R.eval("par(cex.axis = 0.75)");
+		
+		// Pass R the values to plot
+		R.makeVector("x", x);
+		R.makeVector("y", y);
+		R.makeVector("xBG", xBG);
+		R.makeVector("yBG", yBG);
+		
+		// Plot stuff and adorn the plot
+		R.eval("plot(x, y, xlab='Bead Signal [au]', ylab='Secretion Signal [au]', pch=20, cex=0.25, col='red')");
+		R.eval("points(xBG, yBG, pch=20, cex=0.25, col='black')");
+		R.eval("yint <- " + fit[0]);
+		R.eval("slope <- " + fit[1]);
+		R.eval("abline(v=" + xThresh + ", lty=2, col='gray')");
+		R.eval("abline(h=" + yThresh + ", lty=2, col='gray')");
+		R.eval("abline(yint, slope)");
+		R.eval("mtext(paste('slope=',round(slope,2),', int=', round(yint,0), sep=''), adj=1)");
+		R.eval("mtext(" + R.sQuote("ID=" + id) + ", adj=0)");
+		
+		// Call this to finish writing the plot file.
+		R.endPlot();
+		
+		// Return the path to the plot.
+		return path;
+	}
+	
+}
