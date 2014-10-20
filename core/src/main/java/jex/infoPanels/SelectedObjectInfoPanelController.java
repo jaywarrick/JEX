f98e507ed270d62c022841d82717a224fda492eb
jaywarrick <jaywarrick@Warrick-Work.local>
2014-10-20 18:09:58 -0500

    Whitespace

--- core/src/main/java/jex/infoPanels/SelectedObjectInfoPanelController.java
+++ core/src/main/java/jex/infoPanels/SelectedObjectInfoPanelController.java
@@ -118,7 +118,7 @@ public class SelectedObjectInfoPanelController extends InfoPanelController {
 			super();
 			
 			// Do some connexions
-			viewInArray.addActionListener(this);
+			viewInArray.addActionListener(this);			
 			
 			// Make the gui
 			rebuild();
