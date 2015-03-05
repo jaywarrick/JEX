5ed008d4f512c1b43871e1a8c1d3bc77db3068cb
thuibr <t.huibr@gmail.com>
2015-03-05 11:22:05 -0600

    added skeleton to AutoLocalThreshold.java

--- core/src/main/java/function/plugin/plugins/imageThresholding/AutoLocalThresholding.java
+++ core/src/main/java/function/plugin/plugins/imageThresholding/AutoLocalThresholding.java
@@ -1,7 +1,35 @@
 package function.plugin.plugins.imageThresholding;
 
-import fiji.threshold.Auto_Local_Threshold;;
+import org.scijava.plugin.Plugin;
 
-public class AutoLocalThresholding {
+import Database.DBObjects.JEXEntry;
+import fiji.threshold.Auto_Local_Threshold;
+import function.plugin.mechanism.JEXPlugin;
+import function.plugin.mechanism.MarkerConstants;
+import function.plugin.mechanism.ParameterMarker;
 
+
+@Plugin(
+		type = JEXPlugin.class,
+		name="Auto Local Threshold",
+		menuPath="Image Thresholding > Auto Local Threshold",
+		visible=true,
+		description="Function that allows you to stitch an image ARRAY into a single image using two image alignment objects."
+		)
+public class AutoLocalThresholding extends JEXPlugin{
+
+	/////////// Define Inputs ///////////
+
+	/////////// Define Parameters ///////////
+
+	@ParameterMarker(uiOrder=0, name="Method", description="select algorithm to be applied", ui=MarkerConstants.UI_DROPDOWN, choices={"Bernsen", "Contrast", "Mean", "Median", "MidGrey", "Niblack", "Otsu", "Phansalkar", "Sauvola"}, defaultChoice=0)
+	String startPt;
+
+	@Override
+	public boolean run(JEXEntry optionalEntry) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	//////////Define Outputs ///////////
 }
