3a706729d7cf2358c9ba32faecd84086afca8604
thuibr <t.huibr@gmail.com>
2015-03-26 12:23:22 -0500

    added content to AutoImport

--- core/src/main/java/function/plugin/plugins/Import/AutoImport.java
+++ core/src/main/java/function/plugin/plugins/Import/AutoImport.java
@@ -1,14 +1,40 @@
 package function.plugin.plugins.Import;
 
-import java.io.File;
+import ij.process.ShortProcessor;
+import io.scif.FormatException;
+import io.scif.ImageMetadata;
+import io.scif.Plane;
+import io.scif.Reader;
+import io.scif.SCIFIO;
+import io.scif.config.SCIFIOConfig;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map.Entry;
+import java.util.TreeMap;
+import java.util.Vector;
+
+import jex.statics.JEXDialog;
+import jex.statics.JEXStatics;
+import loci.common.DataTools;
+import logs.Logs;
 import miscellaneous.FileUtility;
+import miscellaneous.SimpleFileFilter;
+import net.imagej.axis.Axes;
+import net.imagej.axis.CalibratedAxis;
 
 import org.scijava.plugin.Plugin;
 
+import tables.Dim;
+import tables.DimTable;
 import tables.DimensionMap;
 import Database.DBObjects.JEXData;
 import Database.DBObjects.JEXEntry;
+import Database.DataWriter.ImageWriter;
+import Database.SingleUserDatabase.JEXWriter;
+import function.plugin.IJ2.IJ2PluginUtility;
 import function.plugin.mechanism.JEXPlugin;
 import function.plugin.mechanism.MarkerConstants;
 import function.plugin.mechanism.OutputMarker;
@@ -34,17 +60,23 @@ public class AutoImport extends JEXPlugin {
 
 	/////////// Define Parameters ///////////
 
-	@ParameterMarker(uiOrder=0, name="Input Directory", description="Location of the multicolor TIFF images", ui=MarkerConstants.UI_FILECHOOSER, defaultText="")
+	@ParameterMarker(uiOrder=0, name="Input Directory/File", description="Location of the multicolor TIFF images", ui=MarkerConstants.UI_FILECHOOSER, defaultText="")
 	String inDir;
-	
-	@ParameterMarker(uiOrder=1, name="File Extension", description="The type of file that is being imported. Default is .tif. Not necessary if importing a single file.", ui=MarkerConstants.UI_TEXTFIELD, defaultText=".tif")
-	int fileExtension;
+
+	@ParameterMarker(uiOrder=1, name="File Extension", description="The type of file that is being imported. Default is tif. Not necessary if importing a single file.", ui=MarkerConstants.UI_TEXTFIELD, defaultText="tif")
+	String fileExtension;
+
+//	@ParameterMarker(uiOrder=2, name="Channel Name", description="Name of the channel (e.g. time). Default is \"automatic\" channel naming.", ui=MarkerConstants.UI_TEXTFIELD, defaultText="automatic")
+//	String transferNames;
+
+	@ParameterMarker(uiOrder=3, name="Name Separator", description="Charactor that separates dimension names in the image name", ui=MarkerConstants.UI_TEXTFIELD, defaultText="_")
+	String separator;
 
 	/////////// Define Outputs ///////////
 
 	@OutputMarker(name="Imported Image", type=MarkerConstants.TYPE_IMAGE, flavor="", description="The imported image object", enabled=true)
 	JEXData output;
-	
+
 	@Override
 	public int getMaxThreads()
 	{
@@ -53,15 +85,143 @@ public class AutoImport extends JEXPlugin {
 
 	@Override
 	public boolean run(JEXEntry optionalEntry) {
-		// TODO Tom (see line below)
-		/* FIGURE OUT IF ONE OR MULTIPLE FILES ARE BEING IMPORTED */
+		// GATHER DATA FROM PARAMETERS
+		// create file object for input directory
 		File filePath = new File(inDir);
-		boolean isDirectory = filePath.isDirectory(); // checks whether user entered a single file or an entire directory
-		
-		
-		
+		//boolean autoNameGathering = transferNames.equals("automatic");
+		boolean autoNameGathering = true;
+
+		DimTable table = null;
+
+
+		// TODO verify that file or directory exists; currently we are assuming
+		// that it does exist
+
+
+
+		// FIGURE OUT IF ONE OR MULTIPLE FILES ARE BEING IMPORTED
+		List<File> pendingImageFiles; // contains either one or multiple image files; depends on whether one file is selected or a whole directory
+
+		if (filePath.isDirectory()) { // multiple files
+			// gather and sort the input files
+			JEXStatics.statusBar.setStatusText("Sorting files to convert. May take minutes...");
+			pendingImageFiles = FileUtility.getSortedFileList(filePath.listFiles(new SimpleFileFilter(new String[] { fileExtension })));
+		}
+		else { // one file
+			// initialize pendingImageFiles and add the single file
+			JEXStatics.statusBar.setStatusText("Adding file "+inDir+" to list.");
+			pendingImageFiles = new Vector<File>();
+			pendingImageFiles.add(filePath);
+		}
+
+
+
+		// TODO
+		TreeMap<DimensionMap,String> multiMap = new TreeMap<DimensionMap,String>();
+		for (File f: pendingImageFiles) {
+
+			if(this.isCanceled())
+			{
+				return false;
+			}
+			// usually x and y coordinate map
+			DimensionMap baseMap = this.getMapFromPath(f.getAbsolutePath(), separator);
+
+			// get reader for image file
+			Reader reader = initializeReader(f.getAbsolutePath());
+			// 	get table from reader		
+			table = getDimTableFromReader(reader, autoNameGathering);
+			if(table == null)
+			{
+				JEXDialog.messageDialog("Function canceled manually OR due to issues with determining dimensions of the image.");
+				return false;
+			}
+
+			Iterator<DimensionMap> itr = table.getMapIterator().iterator();
+			double total = reader.getImageCount() * reader.getPlaneCount(0);
+			double count = 0;
+			JEXStatics.statusBar.setProgressPercentage(0);
+			for (int i = 0; i < reader.getImageCount(); i++) {
+				for (int j = 0; j < reader.getPlaneCount(i); j++) {
+					Plane plane;
+					try
+					{
+						plane = reader.openPlane(i, j);
+					}
+					catch (Exception e)
+					{
+						Logs.log("Couldn't read image " + i + " plane " + j + " in " + f.getAbsolutePath() + ". Skipping to next plane.", Logs.ERROR, this);
+						e.printStackTrace();
+						continue;
+					}
+					ImageMetadata d = plane.getImageMetadata();
+					long[] dims = d.getAxesLengthsPlanar();
+					short[] converted = (short[]) DataTools.makeDataArray(plane.getBytes(), 2, false, d.isLittleEndian());
+					ShortProcessor p = new ShortProcessor((int)dims[0], (int)dims[1], converted, null);
+
+					if(this.isCanceled())
+					{
+						return false;
+					}
+
+					//					// For each image split it if necessary
+					//					if(imRows * imCols > 1)
+					//					{
+					//						TreeMap<DimensionMap,ImageProcessor> splitImages = splitRowsAndCols(p, imRows, imCols, this);
+					//						// The above might return null because of being canceled. Catch cancel condition and move on.
+					//						if(this.isCanceled())
+					//						{
+					//							return false;
+					//						}
+					//						DimensionMap map = itr.next().copy();
+					//						for(Entry<DimensionMap,ImageProcessor> e : splitImages.entrySet())
+					//						{
+					//							String filename = JEXWriter.saveImage(e.getValue());
+					//							map.putAll(e.getKey());
+					//							multiMap.put(map.copy(),filename);
+					//							Logs.log(map.toString() + " :: " + filename, this);
+					//						}
+					//					}
+					//					else
+					//					{
+					//						String filename = JEXWriter.saveImage(p);
+					//						DimensionMap map = itr.next().copy();
+					//						multiMap.put(map,filename);
+					//						Logs.log(map.toString() + " = " + filename, this);
+					//					}
+					// copied from else statement above:
+					String filename = JEXWriter.saveImage(p);
+					DimensionMap map = itr.next().copy();
+					map.putAll(baseMap.copy());
+					multiMap.put(map,filename);
+
+					Logs.log(map.toString() + " = " + filename, this);
+
+
+					JEXStatics.statusBar.setProgressPercentage((int) (100.0 * count / total));
+					count = count + 1;
+				}
+			}
+		}
+
+
+		// OUTPUT PROCESSING
+		output = ImageWriter.makeImageStackFromPaths(output.name, multiMap);
+		if (table != null) {
+			DimTable toSet = new DimTable(multiMap);
+			for(Dim d : table)
+			{
+				toSet.removeDimWithName(d.dimName);
+			}
+			for(Dim d : table)
+			{
+				toSet.add(d.copy());
+			}
+			output.setDimTable(toSet);
+		}
+
 		// TODO Auto-generated method stub
-		return false;
+		return true;
 	}
 
 	/**
@@ -75,14 +235,14 @@ public class AutoImport extends JEXPlugin {
 	public DimensionMap getMapFromPath(String filePath, String separator) {
 		String name = FileUtility.getFileNameWithoutExtension(filePath);
 		String[] names = name.split(separator);
-		
+
 		DimensionMap dimMap = new DimensionMap();
 		String dimValue, dimName, temp;
 		int splitIndex = 0;
-		
+
 		for (int i = 0; i < names.length; i++){
 			temp = names[i];
-			
+
 			// find the first Digit in the string in order to separate dimName and dimValue
 			for (int j = 0; j < temp.length(); j++){
 				if (Character.isDigit(temp.charAt(j))){
@@ -92,18 +252,122 @@ public class AutoImport extends JEXPlugin {
 				else
 					splitIndex = 0;
 			}
-			
+
 			// if the string is not a dimName followed by a dimValue then skip it.
 			if (splitIndex != 0) {
 				dimName = temp.substring(0, splitIndex);
 				dimValue = temp.substring(splitIndex);
-				
+
 				dimMap.put(dimName, dimValue);
 			}
 		}
-		
+
 		return dimMap;
-		
+
 	}
-	
+
+	/**
+	 * Initializes a new reader for a specific file
+	 * 
+	 * @param filePath the file's location
+	 * @return 
+	 */
+	private Reader initializeReader(String filePath) {
+		final SCIFIO scifio = new SCIFIO(IJ2PluginUtility.ij.getContext());
+		Reader reader;
+		try
+		{
+			reader = scifio.initializer().initializeReader(filePath, new SCIFIOConfig().checkerSetOpen(true));
+		}
+		catch (Exception e)
+		{
+			Logs.log("Couldn't initialize ND2 file reader for file " + filePath, Logs.ERROR, this);
+			e.printStackTrace();
+			return null;
+		}
+		return reader;
+	}
+
+	private static DimTable getDimTableFromReader(Reader r, boolean transferNames)
+	{
+		DimTable ret = new DimTable();
+		try
+		{
+			ImageMetadata meta = r.openPlane(0, 0).getImageMetadata();
+			long[] lengths = meta.getAxesLengthsNonPlanar();
+			List<CalibratedAxis> axes = meta.getAxesNonPlanar();
+			for(int i = lengths.length-1; i > -1; i--)
+			{
+				ret.add(new Dim(axes.get(i).type().getLabel(), (int) lengths[i]));
+			}
+		}
+		catch (FormatException | IOException e)
+		{
+			e.printStackTrace();
+			return null; // Cancels function
+		}
+
+		if(transferNames)
+		{
+			TreeMap<String,String> colors = new TreeMap<String,String>();
+			for(Entry<String,Object> e : r.getMetadata().getTable().entrySet())
+			{
+				if(e.getKey().contains("Name #"))
+				{
+					colors.put(e.getKey().toString().trim(), e.getValue().toString().trim());
+				}
+			}
+			try
+			{
+				String[] colorNames = ((String[]) colors.values().toArray(new String[]{}));
+				Vector<String> colorNamesList = new Vector<String>();
+				int i = ret.indexOfDimWithName(Axes.CHANNEL.getLabel());
+				int size = ret.get(i).size();
+				if(size == colorNames.length-1)
+				{
+					// This is because the first name found in the metadata is usually just the current setting of the scope or something rather than the name of the setting used to capture the first image.
+					for(int j = 1; j < colorNames.length; j++)
+					{
+						colorNamesList.add(colorNames[j]);
+					}
+				}
+				else
+				{ // Just guessing now.
+					for(int j = 0; j < colorNames.length; j++)
+					{
+						colorNamesList.add(colorNames[j]);
+					}
+				}
+				Dim newColorDim = new Dim(Axes.CHANNEL.getLabel(), colorNamesList); // Using a TreeMap and the TreeMap.values() provides and ordered list based on the order of the "Name #x" key from the non-ordered HashMap of the MetaTable
+
+				if(newColorDim.size() > size)
+				{
+					int choice = JEXDialog.getChoice("What should I do?", "The number of colors/channels does not match the number of possible channel names. Should we replace the indices of colors with the supposed names which might not be correct or just leave the indices?", new String[]{"Replace Names Anyway","Leave as Indices"}, 0);
+					if(choice == 0)
+					{
+						newColorDim = new Dim("Color", newColorDim.valuesStartingAt(newColorDim.size() - size)); // Best guess appears to be last named color settings
+						ret.set(i, newColorDim);
+					}
+					else if(choice == -1)
+					{
+						return null; // Cancels function
+					}
+				}
+				else if(newColorDim.size() < size)
+				{
+					JEXDialog.messageDialog("Couldn't find enough color setting names for each color in the image set.\n\nLeaving indices instead of replacing indices with names.");
+				}
+				else
+				{
+					ret.set(i, newColorDim);
+				}
+			}
+			catch (IndexOutOfBoundsException e)
+			{
+				// Just means there is no color dimension
+				// Don't worry about it.
+			}
+		}
+		return ret;
+	}
 }
