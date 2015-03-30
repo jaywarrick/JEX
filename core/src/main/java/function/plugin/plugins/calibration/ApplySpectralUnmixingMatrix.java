07fd02c704fd14555502e2eafee5170cdd92a461
jaywarrick <jaywarrick@dyn-72-33-209-52.uwnet.wisc.edu>
2015-03-30 10:59:22 -0500

    Create functions for spectral unmixing
    The first function calculates the special unmoving matrix. The other
    applies that matrix.

---
+++ core/src/main/java/function/plugin/plugins/calibration/ApplySpectralUnmixingMatrix.java
@@ -0,0 +1,271 @@
+package function.plugin.plugins.calibration;
+
+import ij.ImagePlus;
+import ij.process.FloatProcessor;
+import ij.process.ImageProcessor;
+
+import java.util.TreeMap;
+import java.util.Vector;
+
+import jex.statics.JEXDialog;
+
+import org.scijava.plugin.Plugin;
+
+import tables.Dim;
+import tables.DimTable;
+import tables.DimensionMap;
+import tables.Table;
+import weka.core.converters.JEXTableReader;
+import Database.DBObjects.JEXData;
+import Database.DBObjects.JEXEntry;
+import Database.DataReader.FileReader;
+import Database.DataReader.ImageReader;
+import Database.DataWriter.ImageWriter;
+import Database.SingleUserDatabase.JEXWriter;
+import Jama.Matrix;
+import function.plugin.mechanism.InputMarker;
+import function.plugin.mechanism.JEXPlugin;
+import function.plugin.mechanism.MarkerConstants;
+import function.plugin.mechanism.OutputMarker;
+import function.plugin.mechanism.ParameterMarker;
+
+@Plugin(
+		type = JEXPlugin.class,
+		name="Apply Spectral Unmixing Matrix",
+		menuPath="Calibration",
+		visible=true,
+		description="Treat an image object as a stack, calculate the median/mean of subgroups of images, take the mean of the subgroups, and smooth if desired. Result of the first successful entry is copied to all selected entries if desired."
+		)
+public class ApplySpectralUnmixingMatrix extends JEXPlugin {
+	
+	public static ImagePlus sharedCalibrationImage = null;
+	
+	public ApplySpectralUnmixingMatrix()
+	{}
+	
+	/**
+	 * Returns true if the user wants to allow multithreding
+	 * 
+	 * @return
+	 */
+	/////////// Define Inputs ///////////
+	
+	@InputMarker(name="Image", type=MarkerConstants.TYPE_IMAGE, description="Image to use to generate calibration image.", optional=true)
+	JEXData imageData;
+	
+	@InputMarker(name="Matrix", type=MarkerConstants.TYPE_FILE, description="Spectral unmixing matrix,", optional=true)
+	JEXData matrixData;
+	
+	/////////// Define Parameters ///////////
+	
+	@ParameterMarker(uiOrder=1, name="Channel Dim Name", description="After unmixing, how much to scale the result", ui=MarkerConstants.UI_TEXTFIELD, defaultText="Channel")
+	String channelDimName;
+	
+	@ParameterMarker(uiOrder=1, name="Fluor Dim Name", description="After unmixing, how much to scale the result", ui=MarkerConstants.UI_TEXTFIELD, defaultText="Fluor")
+	String fluorDimName;
+	
+	@ParameterMarker(uiOrder=1, name="Intensity Scaling Factor", description="After unmixing, how much to scale the result", ui=MarkerConstants.UI_TEXTFIELD, defaultText="1.0")
+	double scalingFactor;
+	
+	/////////// Define Outputs ///////////
+	
+	@OutputMarker(name="Unmixed Image", type=MarkerConstants.TYPE_FILE, flavor="", description="The result of unmixing the channels. The resulting intensities (with a scaling factor of 1) represent intensities relative to the calibration images used to create the unmixing matrix and do not account for changes in exposure times etc.", enabled=true)
+	JEXData outputImage;
+	
+	@Override
+	public int getMaxThreads()
+	{
+		return 10;
+	}
+	
+	@Override
+	public boolean run(JEXEntry entry)
+	{
+		
+		// Check the inputs
+		if(imageData == null || !imageData.getTypeName().getType().equals(JEXData.IMAGE))
+		{
+			return false;
+		}
+		
+		if(matrixData == null || !matrixData.getTypeName().getType().equals(JEXData.FILE))
+		{
+			return false;
+		}
+		
+		// Get the data
+		TreeMap<DimensionMap,String> imageMap = ImageReader.readObjectToImagePathTable(imageData);
+		
+		// Prepare a data structure to store the results
+		TreeMap<DimensionMap,String> outputImages = new TreeMap<DimensionMap,String>();
+		
+		// Get the inverse of the mixing matrix (i.e., the unmixing matrix)
+		Table<Double> matrix = JEXTableReader.getNumericTable(FileReader.readFileObject(matrixData));
+		double[][] imMatrix = getInverseMatrix(matrix, channelDimName, fluorDimName);
+		
+		// Prepare a table that doesn't include channel for looping
+		DimTable table = imageData.getDimTable();
+		DimTable channellessTable = table.getSubTable(channelDimName);
+		
+		// For each combination of the other dimensions in this image object
+		for(DimensionMap filter : channellessTable.getMapIterator())
+		{
+			// Gather the image processors for each of the channels
+			Vector<ImageProcessor> data = new Vector<ImageProcessor>();
+			for(DimensionMap map : table.getMapIterator(filter))
+			{
+				data.add((new ImagePlus(imageMap.get(map)).getProcessor()));				
+			}
+			
+			// Convert the channels signals into fluor signals.
+			Vector<FloatProcessor> ret = getFluorSignals(data, imMatrix);
+			
+			// Save the results
+			int i = 0;
+			for(DimensionMap map : table.getMapIterator(filter))
+			{
+				String toSave = JEXWriter.saveImage(ret.get(i));
+				outputImages.put(map, toSave);	
+				i = i + 1;
+			}
+		}
+	
+		// Write the files and save them
+		outputImage = ImageWriter.makeImageStackFromPaths("temp", outputImages);
+		
+		return true;
+	}
+	
+	public double[][] getInverseMatrix(Table<Double> matrixData, String channelDimName, String fluorDimName)
+	{
+		Dim channelDim = matrixData.dimTable.getDimWithName(channelDimName);
+		Dim fluorDim = matrixData.dimTable.getDimWithName(fluorDimName);
+		double[][] mMatrix = new double[channelDim.size()][fluorDim.size()];
+		double[][] imMatrix = new double[fluorDim.size()][channelDim.size()];
+		for(DimensionMap map : matrixData.dimTable.getMapIterator())
+		{
+			int c = channelDim.index(map.get(channelDimName));
+			int f = channelDim.index(map.get(fluorDimName));
+			mMatrix[c][f] = matrixData.getData(map);
+		}
+		Matrix mixingMatrix = new Matrix(mMatrix);
+		imMatrix = mixingMatrix.inverse().getArray();
+		
+		if (mixingMatrix.rank() < fluorDim.size())
+		{
+			JEXDialog.messageDialog("Warning!!! - The rank of the matrix appears insufficient to use to unmix the channels");
+		}
+		
+		return imMatrix;
+	}
+	
+	public Vector<FloatProcessor> getFluorSignals(Vector<ImageProcessor> channelSignals, double[][] imMatrix)
+	{
+		// Dimensions of imMatrix should be double[fluors][channels] given the mMatrix should have been double[channels][fluors].
+		
+		int width = channelSignals.get(0).getWidth();
+		int height = channelSignals.get(0).getHeight();
+		int channels = imMatrix.length;
+		int fluors = imMatrix[0].length;
+		
+		// Initialize the return object
+		Vector<FloatProcessor> ret = new Vector<FloatProcessor>();
+		for(int i = 0; i < fluors; i++)
+		{
+			ret.add(new FloatProcessor(width, height));
+		}
+		
+		// Loop through pixel locations and convert channel signals to fluor signals.
+		for(int x=0; x<width; x++)
+		{
+			for(int y=0; y<height; y++)
+			{
+				// Assume the image is already background subtracted
+				// Apply the unmixing matrix
+				for(int f=0; f<fluors; f++)
+				{
+					double value = 0;
+					for(int c=0; c<channels; c++)
+					{
+						value = value + imMatrix[f][c] * channelSignals.get(c).getPixelValue(x, y);
+					}
+					ret.get(f).putPixelValue(x, y, value);
+				}
+			}
+		}
+		
+		return ret;
+	}
+	
+//	public FloatProcessor getThreshMean(FloatProcessor fp, ROIPlus roi)
+//	{
+//		AutoThresholder at = new AutoThresholder();
+//		
+//		int width = fp.getWidth();
+//		int height = fp.getHeight();
+//		for(int x=0; x<width; x++)
+//		{
+//			for(int y=0; y<height; y++)
+//			{
+//				// Assume the image is already background subtracted	
+//				// Apply the unmixing matrix
+//				for(f=0; f<nFluors; f++)
+//				{
+//					value  = fInverseMatrix[f][0] * chPixels[0];
+//					for(c=1; c<nChannels; ++c)
+//					{
+//						value += fInverseMatrix[f][c] * chPixels[c];
+//					}
+//					fluoIPVector[f].putPixelValue(x, y, value);
+//				}
+//			}
+//		}
+//		// Do threshold
+//		FloatProcessor temp = (FloatProcessor) fp.convertToFloat();
+//		FunctionUtility.imAdjust(temp, fp.getMin(), fp.getMax(), 0d, 255d, 1d);
+//		ByteProcessor bp = (ByteProcessor) temp.convertToByte(false);
+//		int[] hist = bp.getHistogram();
+//		double threshold = at.getThreshold(AutoThresholder.OTSU, hist);
+//		if(threshold > 255)
+//		{
+//			threshold = 255;
+//		}
+//		else if(threshold < 0)
+//		{
+//			threshold = 0;
+//		}
+//		bp.threshold((int) threshold);
+//		Rectangle r = null;
+//		Roi ijRoi = null;
+//		if(roi != null)
+//		{
+//			ijRoi = roi.getRoi();
+//			r = roi.pointList.getBounds();
+//		}
+//		else
+//		{
+//			r = new Rectangle(bp.getWidth(), bp.getHeight());
+//		}
+//		
+//		Vector<Double> measurements = new Vector<Double>();
+//		for (int i = r.x; i < r.x + r.width; i++)
+//		{
+//			for (int j = r.y; j < r.y + r.height; j++)
+//			{
+//				if((bp.getPixelValue(i, j) > threshold) && (roi != null && ijRoi.contains(i,j)))
+//				{
+//					measurements.add((double) bp.getPixelValue(i, j));
+//				}
+//			}	
+//		}
+//		
+//		if(measurements.size() > 0)
+//		{
+//			return StatisticsUtility.mean(measurements);
+//		}
+//		else
+//		{
+//			return -1.0;
+//		}
+//	}
+}
---
+++ core/src/main/java/function/plugin/plugins/calibration/MakeSpectralUnmixingMatrix.java
@@ -0,0 +1,208 @@
+package function.plugin.plugins.calibration;
+
+import ij.ImagePlus;
+import ij.process.ByteProcessor;
+import ij.process.FloatProcessor;
+import ij.process.ImageStatistics;
+import image.roi.ROIPlus;
+
+import java.util.TreeMap;
+
+import org.scijava.plugin.Plugin;
+
+import tables.DimTable;
+import tables.DimensionMap;
+import weka.core.converters.JEXTableWriter;
+import Database.DBObjects.JEXData;
+import Database.DBObjects.JEXEntry;
+import Database.DataReader.ImageReader;
+import Database.DataReader.RoiReader;
+import Database.DataWriter.FileWriter;
+import function.plugin.mechanism.InputMarker;
+import function.plugin.mechanism.JEXPlugin;
+import function.plugin.mechanism.MarkerConstants;
+import function.plugin.mechanism.OutputMarker;
+import function.plugin.mechanism.ParameterMarker;
+
+@Plugin(
+		type = JEXPlugin.class,
+		name="Make Spectral Unmixing Matrix",
+		menuPath="Calibration",
+		visible=true,
+		description="Treat an image object as a stack, calculate the median/mean of subgroups of images, take the mean of the subgroups, and smooth if desired. Result of the first successful entry is copied to all selected entries if desired."
+		)
+public class MakeSpectralUnmixingMatrix extends JEXPlugin {
+	
+	public static ImagePlus sharedCalibrationImage = null;
+	
+	public MakeSpectralUnmixingMatrix()
+	{}
+	
+	/**
+	 * Returns true if the user wants to allow multithreding
+	 * 
+	 * @return
+	 */
+	/////////// Define Inputs ///////////
+	
+	@InputMarker(name="Background Image (optional)", type=MarkerConstants.TYPE_IMAGE, description="Image with representative background intensities in the imaging channels of interest. Must match those in 'Isolated Fluor Images'.", optional=true)
+	JEXData bgData;
+	
+	@InputMarker(name="Background Roi (optional)", type=MarkerConstants.TYPE_ROI, description="ROI to localize analysis of background within the 'Background Image' or the 'Isolated Fluor Images' if no background image is provided.", optional=true)
+	JEXData bgRoiData;
+	
+	@InputMarker(name="Isolated Fluor Images", type=MarkerConstants.TYPE_IMAGE, description="Images of each isolated fluorophore in each imaging channel.", optional=false)
+	JEXData calData;
+	
+	@InputMarker(name="Isolated Fluor Image Masks (optional)", type=MarkerConstants.TYPE_IMAGE, description="Mask to localize analysis of fluorescent intensities in the 'Isolated Fluor Images'.", optional=true)
+	JEXData calMaskData;
+	
+	/////////// Define Parameters ///////////
+	
+	@ParameterMarker(uiOrder=1, name="Channel Dim Name", description="Name of the dimension describing the imaging channels", ui=MarkerConstants.UI_TEXTFIELD, defaultText="Channel")
+	String channelDimName;
+	
+	@ParameterMarker(uiOrder=1, name="Fluor Dim Name", description="Name of the dimension describing the fluorophores", ui=MarkerConstants.UI_TEXTFIELD, defaultText="Fluor")
+	String fluorDimName;
+	
+	/////////// Define Outputs ///////////
+	
+	@OutputMarker(name="Background Matrix (BG)", type=MarkerConstants.TYPE_FILE, flavor="", description="The mean background intensities ", enabled=true)
+	JEXData bgMatrix;
+	
+	@OutputMarker(name="Signal Matrix (SIG)", type=MarkerConstants.TYPE_FILE, flavor="", description="The full signal cross-talk/mixing matrix", enabled=true)
+	JEXData calMatrix;
+	
+	@OutputMarker(name="Mixing Matrix (A=SIG-BG in p=Ax)", type=MarkerConstants.TYPE_FILE, flavor="", description="The background corrected cross-talk/mixing matrix", enabled=true)
+	JEXData unmixingMatrix;
+	
+	@Override
+	public int getMaxThreads()
+	{
+		return 1;
+	}
+	
+	@Override
+	public boolean run(JEXEntry entry)
+	{
+		
+		// Check required inputs
+		if(calData == null || !calData.getTypeName().getType().equals(JEXData.IMAGE))
+		{
+			return false;
+		}
+		
+		// Check optional inputs
+		if(calMaskData != null && !calData.getTypeName().getType().equals(JEXData.IMAGE))
+		{
+			return false;
+		}
+		if(bgData != null && !bgData.getTypeName().getType().equals(JEXData.IMAGE))
+		{
+			return false;
+		}
+		if(bgRoiData != null && !bgRoiData.getTypeName().getType().equals(JEXData.ROI))
+		{
+			return false;
+		}
+		
+		// Prepare a data structure to store the results
+		TreeMap<DimensionMap,Double> bgMeans = new TreeMap<DimensionMap,Double>();
+		TreeMap<DimensionMap,Double> sigMeans = new TreeMap<DimensionMap,Double>();
+		TreeMap<DimensionMap,Double> unmixingMeans = new TreeMap<DimensionMap,Double>();
+		
+		// First calculate the amount of intensity in each channel of the background image
+		TreeMap<DimensionMap,String> bgImages;
+		
+		if(bgData == null)
+		{
+			bgImages = ImageReader.readObjectToImagePathTable(calData);
+		}
+		else
+		{
+			bgImages = ImageReader.readObjectToImagePathTable(bgData);
+		}
+		
+		DimTable table = calData.getDimTable();
+		TreeMap<DimensionMap,ROIPlus> bgRois = new TreeMap<DimensionMap,ROIPlus>();
+		if(bgRoiData != null)
+		{
+			bgRois = RoiReader.readObjectToRoiMap(bgRoiData);
+		}
+		
+		for(DimensionMap map : table.getDimensionMaps())
+		{
+			ROIPlus roi = bgRois.get(map);
+			ImagePlus bgImage = new ImagePlus(bgImages.get(map));
+			FloatProcessor fp = bgImage.getProcessor().convertToFloatProcessor();
+			
+			if(bgRois.size() > 0)
+			{
+				// Only use images that have a roi defined
+				if(roi != null)
+				{
+					fp.setRoi(roi.getRoi());
+					ImageStatistics stats = fp.getStatistics();
+					
+					// We want a result that only has a color dimension
+					DimensionMap toAdd = new DimensionMap();
+					toAdd.put(channelDimName, map.get(channelDimName));
+					bgMeans.put(toAdd, stats.mean);
+				}
+			}
+			else
+			{
+				// Use all images but store only 1 value per color
+				ImageStatistics stats = fp.getStatistics();
+				
+				// We want a result that only has a color dimension
+				DimensionMap toAdd = new DimensionMap();
+				toAdd.put(channelDimName, map.get(channelDimName));
+				bgMeans.put(toAdd, stats.mean);
+			}
+			bgImage.flush();
+		}
+		
+		// Now we have the gackground intensity for each color that we can subtract from each image of each dye in each color
+		// But first we need to get mean intensities for each color of each calibration image.
+		TreeMap<DimensionMap,String> calImages = ImageReader.readObjectToImagePathTable(calData);
+		TreeMap<DimensionMap,String> calMasks = new TreeMap<DimensionMap,String>();
+		if(calMaskData != null)
+		{
+			calMasks = ImageReader.readObjectToImagePathTable(calMaskData);
+		}
+		
+		ByteProcessor mask;
+		for(DimensionMap map : table.getDimensionMaps())
+		{
+			ImagePlus calImage = new ImagePlus(calImages.get(map));
+			FloatProcessor fp = calImage.getProcessor().convertToFloatProcessor();
+			
+			if(calMasks.size() > 0)
+			{
+				mask = (ByteProcessor) (new ImagePlus(calMasks.get(map))).getProcessor(); 
+				fp.setMask(mask);
+			}
+			
+			double mean = fp.getStatistics().mean;
+			sigMeans.put(map, mean);
+		}
+		
+		// Now we subtract background intensities from the the full signal intensities
+		for(DimensionMap map : table.getDimensionMaps())
+		{
+			unmixingMeans.put(map, sigMeans.get(map) - bgMeans.get(map));
+		}		
+		
+		// Write the files and save them
+		String bgPath = JEXTableWriter.writeTable("Background", bgMeans);
+		String sigPath = JEXTableWriter.writeTable("Signal", sigMeans);
+		String unmixingPath = JEXTableWriter.writeTable("UnmixingMatrix", unmixingMeans);
+		
+		bgMatrix = FileWriter.makeFileObject("temp", null, bgPath);
+		calMatrix = FileWriter.makeFileObject("temp", null, sigPath);
+		unmixingMatrix = FileWriter.makeFileObject("temp", null, unmixingPath);
+		
+		return true;
+	}
+}
