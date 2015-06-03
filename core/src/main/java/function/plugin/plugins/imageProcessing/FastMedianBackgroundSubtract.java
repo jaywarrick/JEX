f96e143976e9179ab9b220d1fb05a028adfda9eb
jaywarrick <jaywarrick@dyn-72-33-209-188.uwnet.wisc.edu>
2015-06-03 11:12:02 -0500

    Add Fast Median Filter Helper files
    These are used by the CTC Plugin for fast median background subtraction
    that will eventually be ported to JEX as well.

---
+++ core/src/main/java/function/plugin/plugins/imageProcessing/FastMedianBackgroundSubtract.java
@@ -0,0 +1,137 @@
+package function.plugin.plugins.imageProcessing;
+
+import ij.ImagePlus;
+import ij.process.Blitter;
+import ij.process.FloatProcessor;
+import ij.process.ImageProcessor;
+
+import java.io.File;
+import java.util.TreeMap;
+
+import jex.statics.JEXStatics;
+import jex.utilities.FunctionUtility;
+
+import org.scijava.plugin.Plugin;
+
+import tables.DimensionMap;
+import Database.DBObjects.JEXData;
+import Database.DBObjects.JEXEntry;
+import Database.DataReader.ImageReader;
+import Database.DataWriter.ImageWriter;
+import Database.SingleUserDatabase.JEXWriter;
+import function.plugin.mechanism.InputMarker;
+import function.plugin.mechanism.JEXPlugin;
+import function.plugin.mechanism.MarkerConstants;
+import function.plugin.mechanism.OutputMarker;
+import function.plugin.mechanism.ParameterMarker;
+import function.plugin.plugins.medianFilterHelpers.FastMedian;
+
+/**
+ * This is a JEXperiment function template To use it follow the following instructions
+ * 
+ * 1. Fill in all the required methods according to their specific instructions 2. Place the file in the Functions/SingleDataPointFunctions folder 3. Compile and run JEX!
+ * 
+ * JEX enables the use of several data object types The specific API for these can be found in the main JEXperiment folder. These API provide methods to retrieve data from these objects, create new objects and handle the data they contain.
+ * 
+ */
+
+@Plugin(
+		type = JEXPlugin.class,
+		name="Fast Median Background Subtraction",
+		menuPath="Image Processing",
+		visible=true,
+		description="Fast median filter background subtraction that uses a square shaped kernal instead of round like ImageJ's filters. (~> 20 times faster)"
+		)
+public class FastMedianBackgroundSubtract extends JEXPlugin {
+
+	public FastMedianBackgroundSubtract()
+	{}
+	
+	/////////// Define Inputs ///////////
+	
+	@InputMarker(uiOrder=1, name="Image", type=MarkerConstants.TYPE_IMAGE, description="Image to be adjusted.", optional=false)
+	JEXData imageData;
+	
+	/////////// Define Parameters ///////////
+	
+	@ParameterMarker(uiOrder=1, name="Kernal Width", description="Pixel width of the kernal", ui=MarkerConstants.UI_TEXTFIELD, defaultText="5")
+	int kernalWidth;
+	
+	@ParameterMarker(uiOrder=1, name="Nominal Value to Add Back", description="Nominal value to add to all pixels after background subtraction because some image formats don't allow negative numbers.", ui=MarkerConstants.UI_TEXTFIELD, defaultText="100")
+	double nominal;
+	
+	/////////// Define Outputs ///////////
+	
+	@OutputMarker(uiOrder=1, name="Adjusted Image", type=MarkerConstants.TYPE_IMAGE, flavor="", description="The resultant adjusted image", enabled=true)
+	JEXData output;
+	
+	@Override
+	public int getMaxThreads()
+	{
+		return 10;
+	}
+
+	@Override
+	public boolean run(JEXEntry optionalEntry)
+	{
+		// Validate the input data
+		if(imageData == null || !imageData.getTypeName().getType().equals(JEXData.IMAGE))
+		{
+			return false;
+		}
+		
+		// Run the function
+		TreeMap<DimensionMap,String> imageMap = ImageReader.readObjectToImagePathTable(imageData);
+		TreeMap<DimensionMap,String> outputImageMap = new TreeMap<DimensionMap,String>();
+		int count = 0, percentage = 0;
+		String tempPath;
+		for (DimensionMap map : imageMap.keySet())
+		{
+			// Call helper method
+			ImageProcessor ip = (new ImagePlus(imageMap.get(map))).getProcessor();
+			ImageProcessor ip2 = FastMedian.process(ip, kernalWidth);
+			ip2.subtract(nominal);
+			ip.copyBits(ip2, 0, 0, Blitter.SUBTRACT);
+			tempPath = JEXWriter.saveImage(ip);
+			if(tempPath != null)
+			{
+				outputImageMap.put(map, tempPath);
+			}
+			count = count + 1;
+			percentage = (int) (100 * ((double) (count) / ((double) imageMap.size())));
+			JEXStatics.statusBar.setProgressPercentage(percentage);
+		}
+		if(outputImageMap.size() == 0)
+		{
+			return false;
+		}
+		
+		this.output = ImageWriter.makeImageStackFromPaths("temp",outputImageMap);
+		
+		// Return status
+		return true;
+	}
+	
+	public static String saveAdjustedImage(String imagePath, double oldMin, double oldMax, double newMin, double newMax, double gamma, int bitDepth)
+	{
+		// Get image data
+		File f = new File(imagePath);
+		if(!f.exists())
+		{
+			return null;
+		}
+		ImagePlus im = new ImagePlus(imagePath);
+		FloatProcessor imp = (FloatProcessor) im.getProcessor().convertToFloat(); // should be a float processor
+		
+		// Adjust the image
+		FunctionUtility.imAdjust(imp, oldMin, oldMax, newMin, newMax, gamma);
+		
+		// Save the results
+		ImagePlus toSave = FunctionUtility.makeImageToSave(imp, "false", bitDepth);
+		String imPath = JEXWriter.saveImage(toSave);
+		im.flush();
+		
+		// return the filepath
+		return imPath;
+	}
+}
