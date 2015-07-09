c6236ccca32d773a7a9f42bd7b7942ea3198eda4
thuibr <t.huibr@gmail.com>
2015-07-09 11:06:46 -0500

    did some stuff

--- core/src/main/java/function/plugin/plugins/Export/RunCellProfiler.java
+++ core/src/main/java/function/plugin/plugins/Export/RunCellProfiler.java
@@ -151,6 +151,9 @@ public class RunCellProfiler extends JEXPlugin {
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 
 
