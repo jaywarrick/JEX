12eb0a2ce5947e6c1b0bd8855a372499d5fdbb3d
jaywarrick <jaywarrick@Warrick-Work.local>
2015-07-17 10:29:54 -0500

    Add convenience functions for writing line by line with DimensionMaps

--- core/src/main/java/miscellaneous/JEXCSVWriter.java
+++ core/src/main/java/miscellaneous/JEXCSVWriter.java
@@ -4,11 +4,29 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.List;
+import java.util.Map.Entry;
+
+import tables.DimTable;
+import tables.DimensionMap;
+import weka.core.converters.JEXTableWriter;
 
 public class JEXCSVWriter {
 	
 	public String path;
 	public CSVWriter writer;
+	
+	public JEXCSVWriter()
+	{
+		try
+		{
+			this.path = DirectoryManager.getUniqueAbsoluteTempPath("csv");
+			this.writer = new CSVWriter(new FileWriter(new File(this.path)));
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+	}
 	
 	public JEXCSVWriter(String path)
 	{
@@ -21,6 +39,46 @@ public class JEXCSVWriter {
 		{
 			e.printStackTrace();
 		}
+	}
+	
+	public void write(DimensionMap map, String value)
+	{
+		String[] row = new String[map.size() + 1];
+		int i = 0;
+		for(Entry<String,String> e : map.entrySet())
+		{
+			row[i] = e.getValue();
+			i = i + 1;
+		}
+		row[map.size()] = value;
+		
+		this.write(row);
+	}
+	
+	/**
+	 * Will write header of with values equal to keys of the map. It will
+	 * also add a "VALUE" column based on JEXTableWriter.VALUE.
+	 * @param map
+	 */
+	public void writeHeader(DimensionMap map)
+	{
+		String[] row = new String[map.size() + 1];
+		
+		int i = 0;
+		for(Entry<String,String> e : map.entrySet())
+		{
+			row[i] = e.getKey();
+			i = i + 1;
+		}
+		row[map.size()] = JEXTableWriter.VALUE;
+		this.write(row);
+	}
+	
+	public void writeHeader(DimTable dimTable)
+	{
+		// If we do it this way then the order of the dims will match the order in the DimensionMaps
+		// when a row is written.
+		this.writeHeader(dimTable.getMapIterator().iterator().next());
 	}
 	
 	public void write(List<String> rowOfInfo)
