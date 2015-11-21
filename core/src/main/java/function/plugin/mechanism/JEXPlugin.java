7032be6efca071bbe0fa17b84ebb839e06db90aa
jaywarrick <jaywarrick@Warrick-Work.local>
2015-11-21 13:45:03 -0600

    Add convenience functions for checking input validity
    This is to parallel the "identical" function for JEXCrunchable

--- core/src/main/java/function/plugin/mechanism/JEXPlugin.java
+++ core/src/main/java/function/plugin/mechanism/JEXPlugin.java
@@ -1,12 +1,15 @@
 package function.plugin.mechanism;
 
+import jex.statics.JEXDialog;
 import jex.statics.JEXStatics;
 import miscellaneous.Cancelable;
 import miscellaneous.Canceler;
 
 import org.scijava.plugin.SciJavaPlugin;
 
+import Database.DBObjects.JEXData;
 import Database.DBObjects.JEXEntry;
+import Database.Definition.Type;
 import cruncher.Ticket;
 
 public abstract class JEXPlugin implements SciJavaPlugin, Canceler, Cancelable {
@@ -49,5 +52,22 @@ public abstract class JEXPlugin implements SciJavaPlugin, Canceler, Cancelable {
 	public boolean isCanceled()
 	{
 		return this.canceler.isCanceled();
+	}
+	
+	public static boolean isInputValid(JEXData data, Type type)
+	{
+		if(data == null || !data.getTypeName().getType().equals(type))
+		{
+			if(data == null)
+			{
+				JEXDialog.messageDialog("A required object of type '" + type.toString() + " 'is missing! Aborting.");
+			}
+			else
+			{
+				JEXDialog.messageDialog(data.getTypeName().getName() + " was expected to be a " + type.toString() + " but is a " + data.getTypeName().getType().toString() + ". Aborting.");
+			}
+			return false;
+		}
+		return true;
 	}
 }
