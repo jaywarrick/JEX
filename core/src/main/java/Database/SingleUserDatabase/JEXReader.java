39a9a440af2f05d0ebb8e00b48f00e97d6efaaa6
jaywarrick <jaywarrick@Warrick-Work.local>
2015-11-21 13:42:03 -0600

    Add convenience functions for opening Img<> images

--- core/src/main/java/Database/SingleUserDatabase/JEXReader.java
+++ core/src/main/java/Database/SingleUserDatabase/JEXReader.java
@@ -1,12 +1,21 @@
 package Database.SingleUserDatabase;
 
+import function.plugin.IJ2.IJ2PluginUtility;
+import io.scif.img.ImgIOException;
+import io.scif.img.ImgOpener;
+import io.scif.img.SCIFIOImgPlus;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 
+import jex.statics.JEXDialog;
 import miscellaneous.CSVReader;
+import net.imglib2.img.Img;
+import net.imglib2.type.numeric.RealType;
+import net.imglib2.type.numeric.integer.UnsignedByteType;
 
 public class JEXReader {
 	
@@ -34,6 +43,43 @@ public class JEXReader {
 		}
 		
 		return ret;
+	}
+	
+	public static  SCIFIOImgPlus<UnsignedByteType> getByteImage(String path)
+	{
+		ImgOpener imgOpener = new ImgOpener(IJ2PluginUtility.ij().getContext());
+
+		// Always get the first image because that is how JEX stores images.
+		SCIFIOImgPlus<UnsignedByteType> img;
+		try
+		{
+			img = (SCIFIOImgPlus<UnsignedByteType>) imgOpener.openImgs(path, new UnsignedByteType()).get(0);
+			return img;
+		} catch(ImgIOException e)
+		{
+			JEXDialog.messageDialog("Couldn't open file as 8-bit image. Check input images. File = " + path );
+			e.printStackTrace();
+			return null;
+		}		
+	}
+	
+	@SuppressWarnings("unchecked")
+	public static <T extends RealType<T>> Img<T> getSingleImage(String path)
+	{
+		ImgOpener imgOpener = new ImgOpener(IJ2PluginUtility.ij().getContext());
+
+		// Always get the first image because that is how JEX stores images.
+		Img<T> img;
+		try
+		{
+			img = (Img<T>) imgOpener.openImgs(path).get(0);
+			return img;
+		} catch(ImgIOException e)
+		{
+			JEXDialog.messageDialog("Couldn't open file as image. Check input images. File = " + path );
+			e.printStackTrace();
+			return null;
+		}		
 	}
 	
 }
