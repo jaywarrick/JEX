39e203252be9fbf070716fc14e0aab3f18c83976
jaywarrick <warrick@wisc.edu>
2016-01-05 15:24:54 -0600

    Improve error messages on ImageStackStitcher

--- core/src/main/java/function/plugin/plugins/imageTools/ImageStackStitcher.java
+++ core/src/main/java/function/plugin/plugins/imageTools/ImageStackStitcher.java
@@ -60,63 +60,63 @@ import function.plugin.old.JEX_ImageTools_Stitch_Coord;
 		description="Function that allows you to stitch an image STACK into a single image using two image alignment objects."
 		)
 public class ImageStackStitcher extends JEXPlugin {
-	
+
 	/////////// Define Inputs ///////////
-	
+
 	@InputMarker(uiOrder=1, name="H-Alignment", type=MarkerConstants.TYPE_VALUE, description="Horizontal alignment (L to R adjacent images) object obtained from the image aligner plugin on the plugins tab of JEX.", optional=false)
 	JEXData hData;
-	
+
 	@InputMarker(uiOrder=2, name="V-Alignment", type=MarkerConstants.TYPE_VALUE, description="Vertical alignment (Top to Bottom adjeacent images) object obtained from the image aligner plugin on the plugins tab of JEX.", optional=false)
 	JEXData vData;
-	
+
 	@InputMarker(uiOrder=3, name="Images", type=MarkerConstants.TYPE_IMAGE, description="Image to be adjusted.", optional=false)
 	JEXData imageData;
-	
-	
+
+
 	/////////// Define Parameters ///////////
-	
+
 
 	@ParameterMarker(uiOrder=-1, name="Location Dim Name", description="Name of the row dimension in the imageset.", ui=MarkerConstants.UI_TEXTFIELD, defaultText="Location")
 	String locDimName;
-	
+
 	@ParameterMarker(uiOrder=0, name="Number of Columns", description="Number of columns that make up the stitched image. (Can also specify a label name)", ui=MarkerConstants.UI_TEXTFIELD, defaultText="2")
 	String colsString;
-	
+
 	@ParameterMarker(uiOrder=1, name="Starting Point", description="In what corner is the first image of each image group to be stitched.", ui=MarkerConstants.UI_DROPDOWN, choices={"UL", "UR", "LL", "LR"}, defaultChoice=0)
 	String startPt;
-	
+
 	@ParameterMarker(uiOrder=2, name="Horizontal First Movement?", description="From the start point, which direction is the next image, (checked = horizontal, unchecked = vertical)", ui=MarkerConstants.UI_CHECKBOX, defaultBoolean=true)
 	boolean horizontal;
-	
+
 	@ParameterMarker(uiOrder=3, name="Snaking Path?", description="From the start point, which direction is the next image", ui=MarkerConstants.UI_CHECKBOX, defaultBoolean=false)
 	boolean snaking;
-	
+
 	@ParameterMarker(uiOrder=4, name="Size Scale", description="How much to multiply the image size (0.5 reduces image to half its original size)", ui=MarkerConstants.UI_TEXTFIELD, defaultText="1.0")
 	double scale;
-	
+
 	@ParameterMarker(uiOrder=5, name="Intensity Multiplier", description="How much to multiply the image size (0.5 reduces image to half its original size)", ui=MarkerConstants.UI_TEXTFIELD, defaultText="1.0")
 	double multiplier;
-	
+
 	@ParameterMarker(uiOrder=6, name="Output Bit Depth", description="Depth of the outputted image", ui=MarkerConstants.UI_DROPDOWN, choices={ "8", "16", "32" }, defaultChoice=1)
 	int bitDepth;
-	
+
 	@ParameterMarker(uiOrder=7, name="Normalize Intensities Fit Bit Depth", description="Scale intensities to go from 0 to max value determined by new bit depth (\'true\' overrides intensity multiplier).", ui=MarkerConstants.UI_CHECKBOX, defaultBoolean=false)
 	boolean normalize;
-	
-	
+
+
 	////////// Define Outputs ///////////
-	
+
 	@OutputMarker(uiOrder=1, name="Stitched Image", type=MarkerConstants.TYPE_IMAGE, flavor="", description="The resultant stitched image", enabled=true)
 	JEXData output;
 
 	public ImageStackStitcher()
 	{}
-	
+
 	// ----------------------------------------------------
 	// --------- INFORMATION ABOUT THE FUNCTION -----------
 	// ----------------------------------------------------
-	
-	
+
+
 	/**
 	 * Returns true if the user wants to allow multithreding
 	 * 
@@ -125,13 +125,13 @@ public class ImageStackStitcher extends JEXPlugin {
 	@Override
 	public int getMaxThreads()
 	{
-		return 10;
+		return 1;
 	}
-	
+
 	// ----------------------------------------------------
 	// --------- THE ACTUAL MEAT OF THIS FUNCTION ---------
 	// ----------------------------------------------------
-	
+
 	/**
 	 * Perform the algorithm here
 	 * 
@@ -147,7 +147,7 @@ public class ImageStackStitcher extends JEXPlugin {
 		CSVList alignmentInfoHor = new CSVList(ValueReader.readValueObject(hData));
 		int horDxImage = Integer.parseInt(alignmentInfoHor.get(0));
 		int horDyImage = Integer.parseInt(alignmentInfoHor.get(1));
-		
+
 		if(vData == null || !vData.getTypeName().getType().equals(JEXData.VALUE))
 		{
 			return false;
@@ -155,15 +155,21 @@ public class ImageStackStitcher extends JEXPlugin {
 		CSVList alignmentInfoVer = new CSVList(ValueReader.readValueObject(vData));
 		int verDxImage = Integer.parseInt(alignmentInfoVer.get(0));
 		int verDyImage = Integer.parseInt(alignmentInfoVer.get(1));
-		
+
 		// Collect the inputs
-		if(imageData == null || !imageData.getTypeName().getType().equals(JEXData.IMAGE))
+		if(imageData == null)
 		{
-			JEXDialog.messageDialog("Couldn't find a location dimension name with the name '" + locDimName + "'");
+			JEXDialog.messageDialog("No object by that name exists for this entry: X" + entry.getTrayX() + " Y" + entry.getTrayY());
+			return false;
+		}
+		// Collect the inputs
+		if(!imageData.getTypeName().getType().equals(JEXData.IMAGE))
+		{
+			JEXDialog.messageDialog("The object provided is not an 'Image' object.");
 			return false;
 		}
 		DimTable table = imageData.getDimTable();
-		
+
 		// Get the number of columns
 		Integer cols = -1;
 		JEXData colsLabel = null;
@@ -187,17 +193,18 @@ public class ImageStackStitcher extends JEXPlugin {
 				JEXDialog.messageDialog("Couldn't parse '" + colsString + "' to a number, so looked and found a corresponding label but couldn't parse the label value to an integer. Label value: " + LabelReader.readLabelValue(colsLabel));
 			}
 		}
-		
+
 		Dim locDim = table.getDimWithName(locDimName);
 		if(locDim == null)
 		{
+			JEXDialog.messageDialog("No dimension called '" + locDimName + "' in the object for this entry: X" + entry.getTrayX() + " Y" + entry.getTrayY());
 			return false;
 		}
 		int rows = locDim.size()/cols;
-		
+
 		// Run the function
 		PointList imageCoords = this.getMovements(horDxImage, horDyImage, verDxImage, verDyImage, rows, cols, horizontal, snaking, startPt, scale);
-		
+
 		// Remove the row and col Dim's from the DimTable and iterate through it
 		// and stitch.
 		table.remove(locDim);
@@ -219,15 +226,15 @@ public class ImageStackStitcher extends JEXPlugin {
 				e.printStackTrace();
 				return false;
 			}
-			
+
 		}
-		
+
 		output = ImageWriter.makeImageStackFromPaths("temp", stitchedImageFilePaths);
-		
+
 		// Return status
 		return true;
 	}
-	
+
 	private List<DimensionMap> getMapsForStitching(Dim locDim, DimensionMap partialMap)
 	{
 		List<DimensionMap> ret = new Vector<DimensionMap>();
@@ -239,19 +246,19 @@ public class ImageStackStitcher extends JEXPlugin {
 		}
 		return ret;
 	}
-	
+
 	private PointList getMovements(int horDxImage, int horDyImage, int verDxImage, int verDyImage, int rows, int cols, boolean horizontal, boolean snaking, String startPt, double scale)
 	{
-		
+
 		PointList ret = new PointList();
 		Vector<Integer> myRows = this.getIndices(rows, false);
 		Vector<Integer> myRowsRev = this.getIndices(rows, true);
 		Vector<Integer> myCols = this.getIndices(cols, false);
 		Vector<Integer> myColsRev = this.getIndices(cols, true);
-		
+
 		Vector<Integer> theRows = myRows;
 		Vector<Integer> theCols = myCols;
-		
+
 		if(startPt.equals("LL") || startPt.equals("LR"))
 		{
 			theRows = myRowsRev;
@@ -260,7 +267,7 @@ public class ImageStackStitcher extends JEXPlugin {
 		{
 			theCols = myColsRev;
 		}
-		
+
 		if(!snaking && horizontal)
 		{
 			for (int r : theRows)
@@ -317,7 +324,7 @@ public class ImageStackStitcher extends JEXPlugin {
 				}
 			}
 		}
-		
+
 		// Scale and put bounding rectangle at 0,0
 		ret.scale(scale);
 		Rectangle rect = ret.getBounds();
@@ -325,7 +332,7 @@ public class ImageStackStitcher extends JEXPlugin {
 		System.out.println(ret.getBounds());
 		return ret;
 	}
-	
+
 	private Vector<Integer> getIndices(int count, boolean reversed)
 	{
 		Vector<Integer> dim = new Vector<Integer>();
@@ -345,7 +352,7 @@ public class ImageStackStitcher extends JEXPlugin {
 		}
 		return dim;
 	}
-	
+
 	public static File stitch(JEXEntry entry, JEXData imageData, List<DimensionMap> imageDimMaps, PointList imageDisplacements, double scale, boolean normalize, double multiplier, int bits)
 	{
 		// /// prepare a blank image on which to copy the others
@@ -358,7 +365,7 @@ public class ImageStackStitcher extends JEXPlugin {
 		FloatProcessor stitchIP = null;
 		stitchIP = new FloatProcessor(totalWidth, totalHeight);
 		ImagePlus stitch = new ImagePlus("Stitch", stitchIP);
-		
+
 		PointList xy = imageDisplacements;
 		int count = 0;
 		Iterator<DimensionMap> itr = imageDimMaps.iterator();
@@ -376,7 +383,7 @@ public class ImageStackStitcher extends JEXPlugin {
 			// be a
 			// float
 			// processor
-			
+
 			// //// Begin Actual Function
 			if(scale != 1.0)
 			{
@@ -387,28 +394,28 @@ public class ImageStackStitcher extends JEXPlugin {
 			// System.out.println(p);
 			stitchIP.copyBits(imp, p.x, p.y, Blitter.COPY);
 			// //// End Actual Function
-			
+
 			count = count + 1;
 			percentage = (int) (100 * ((count) / ((double) imageDimMaps.size() + 1)));
 			JEXStatics.statusBar.setProgressPercentage(percentage);
 			im.flush();
 		}
-		
+
 		// //// Save the results
 		stitch.setProcessor("Stitch", stitchIP);
 		count = count + 1;
 		percentage = (int) (100 * ((count) / ((double) imageDimMaps.size() + 2)));
 		JEXStatics.statusBar.setProgressPercentage(percentage);
-		
+
 		// Save the resulting image file
 		ImagePlus toSave = FunctionUtility.makeImageToSave(stitchIP, normalize, 1.0, bits);
 		String imPath = JEXWriter.saveImage(toSave);
 		File result = new File(imPath);
-		
+
 		count = count + 1;
 		percentage = (int) (100 * ((count) / ((double) imageDimMaps.size() + 2)));
 		JEXStatics.statusBar.setProgressPercentage(percentage);
-		
+
 		return result;
 	}
 }
