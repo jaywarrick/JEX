f6615db2c451e1b5835d8da7e007de2806020634
jaywarrick <warrick@wisc.edu>
2017-04-13 15:20:03 -0500

    Synchronize writing in writer wrapper just in case.

--- core/src/main/java/function/ops/featuresets/wrappers/WriterWrapper.java
+++ core/src/main/java/function/ops/featuresets/wrappers/WriterWrapper.java
@@ -17,69 +17,77 @@ public class WriterWrapper {
 	public JEXCSVWriter writer = null;
 	public Set<String> header = null;
 	
-	public void write(DimensionMap map, Double value) {
-		if (this.writer == null) {
-			this.writer = new JEXCSVWriter();
-			this.writer.writeHeader(map);
+	public static synchronized void write(WriterWrapper writer, DimensionMap map, Double value)
+	{
+		if (writer.writer == null) {
+			writer.writer = new JEXCSVWriter();
+			writer.writer.writeHeader(map);
 		}
 		DimensionMap temp = map.copy();
-		if (header == null) {
-			header = map.copy().keySet();
+		if (writer.header == null) {
+			writer.header = map.copy().keySet();
 		}
-		for (String s : header) {
+		for (String s : writer.header) {
 			if (!map.containsKey(s)) {
 				temp.put(s, "NA");
 			}
 		}
-		writer.write(temp, value.toString());
+		writer.writer.write(temp, value.toString());
 	}
 	
-	public Pair<JEXData,JEXData> close() {
-		Logs.log("Closing the function (closing file writers and converting output file to arff as well).", this);
-		if(this.writer == null)
+	public static synchronized Pair<JEXData,JEXData> close(WriterWrapper writer, boolean saveArff)
+	{
+		Logs.log("Closing the function (closing file writers and converting output file to arff as well).", WriterWrapper.class);
+		if(writer == null || writer.writer == null)
 		{
 			return new Pair<JEXData,JEXData>(null, null);
 		}
-		writer.close();
-		String csvPath = writer.getPath();
-		JEXCSVReader reader = new JEXCSVReader(csvPath, true);
-
-		// In order to write an Arff table we need to build a DimTable
-		// We can't keep all the data in memory as it might be too large so just
-		// build DimTable for now.
-		DimTableBuilder builder = new DimTableBuilder();
-		while (!reader.isEOF()) {
-			Pair<DimensionMap, String> toAdd = reader.readRowToDimensionMapString();
-			if (toAdd != null) {
-				builder.add(toAdd.p1);
-			} else {
-				Logs.log("Went past end of file!?!", this);
-			}
-		}
-		reader.close();
-
-		// Now that we have the DimTable we can transfer each row of the csv to
-		// the arff file after writing the header.
-		reader = new JEXCSVReader(csvPath, true);
-		JEXTableWriter arffWriter = new JEXTableWriter("FeatureTable", "arff");
-		arffWriter.writeNumericTableHeader(builder.getDimTable());
-		while (!reader.isEOF()) {
-			Pair<DimensionMap, String> result = reader.readRowToDimensionMapString();
-			if (result != null) {
-				arffWriter.writeData(result.p1, Double.parseDouble(result.p2));
-			} else {
-				Logs.log("Went past end of file!?!", this);
-			}
-		}
-
-		// Close and save the data.
-		reader.close();
-		String arffPath = arffWriter.getPath();
-		arffWriter.close();
-		JEXData outputCSV = FileWriter.makeFileObject("temp", null, csvPath);
-		JEXData outputARFF = FileWriter.makeFileObject("temp", null, arffPath);
-		return new Pair<JEXData,JEXData>(outputCSV, outputARFF);
+		writer.writer.close();
+		String csvPath = writer.writer.getPath();
 		
-	}
+		JEXData outputARFF = null;
+		if(saveArff)
+		{
+			JEXCSVReader reader = new JEXCSVReader(csvPath, true);
 
+			// In order to write an Arff table we need to build a DimTable
+			// We can't keep all the data in memory as it might be too large so just
+			// build DimTable for now.
+			DimTableBuilder builder = new DimTableBuilder();
+			while (!reader.isEOF()) {
+				Pair<DimensionMap, String> toAdd = reader.readRowToDimensionMapString();
+				if (toAdd != null) {
+					builder.add(toAdd.p1);
+				} else {
+					Logs.log("Went past end of file!?!", WriterWrapper.class);
+				}
+			}
+			reader.close();
+
+			// Now that we have the DimTable we can transfer each row of the csv to
+			// the arff file after writing the header.
+			reader = new JEXCSVReader(csvPath, true);
+			JEXTableWriter arffWriter = new JEXTableWriter("FeatureTable", "arff");
+			arffWriter.writeNumericTableHeader(builder.getDimTable());
+			while (!reader.isEOF()) {
+				Pair<DimensionMap, String> result = reader.readRowToDimensionMapString();
+				if (result != null) {
+					arffWriter.writeData(result.p1, Double.parseDouble(result.p2));
+				} else {
+					Logs.log("Went past end of file!?!", WriterWrapper.class);
+				}
+			}
+
+			// Close and save the data.
+			reader.close();
+			String arffPath = arffWriter.getPath();
+			arffWriter.close();
+			outputARFF = FileWriter.makeFileObject("temp", null, arffPath);
+		}
+		
+		// Always do this at the end (i.e., before trying to make an Arff version as doing so moves the location of the file prior to conversion cause a FileNotFoundException). Here, everything is fine.
+		JEXData outputCSV = FileWriter.makeFileObject("temp", null, csvPath);
+		
+		return new Pair<JEXData,JEXData>(outputCSV, outputARFF);
+	}
 }
