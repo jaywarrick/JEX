06642bd3830fad84475acba1072d168f04aa340b
jaywarrick <warrick@wisc.edu>
2018-04-24 14:47:30 -0500

    Change name of function getJEXData to readJEXData

--- core/src/main/java/plugin/rscripter/RScripter.java
+++ core/src/main/java/plugin/rscripter/RScripter.java
@@ -373,7 +373,7 @@ public class RScripter implements PlugInController, ActionListener, ClipboardOwn
 	{
 		Vector<Pair<String,String>> labels = getLabelsInEntry(e);
 		String listC = getListRCommand(labels);
-		String ret = "getJEXData(dbPath=" + R.sQuote(JEXWriter.getDatabaseFolder()) + ", ds=" + R.sQuote(e.getEntryExperiment()) + ", x=" + e.getTrayX() + ", y=" + e.getTrayY() + ", type=" + R.sQuote(tn.getType().getType()) + ", name=" + R.sQuote(tn.getName()) + ", " + listC + ")";
+		String ret = "readJEXData(dbPath=" + R.sQuote(JEXWriter.getDatabaseFolder()) + ", ds=" + R.sQuote(e.getEntryExperiment()) + ", x=" + e.getTrayX() + ", y=" + e.getTrayY() + ", type=" + R.sQuote(tn.getType().getType()) + ", name=" + R.sQuote(tn.getName()) + ", " + listC + ")";
 		ret = ret.replace("\\", "/"); // R likes forward slashes.
 		return ret;
 	}
